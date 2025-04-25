953e974361e17e4a0256768a6247e8d95a013c02
Paul Goldman <paulgoldmanmusic@gmail.com>
2025-04-25 13:24:56 -0500

    init commit

---
+++ contactsManager.go
@@ -0,0 +1,10 @@
+package gocontactsmanager
+
+type Contact struct {
+	Name string
+	Age  int
+}
+
+func main() {
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/pgTechSolutionsInc/GoContactsManager
+
+go 1.24.2
